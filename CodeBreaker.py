251ccc222343798be52e74aadcc611786f808ecb
BigPoppaG <daniel.gelderd@gmail.com>
2019-06-15 10:29:40 +0100

    Running main script now possible and assertions working

--- CodeBreaker.py
+++ CodeBreaker.py
@@ -1,9 +1,3 @@
-def test():
-    board = Board((1, 2, 1, 3), 5)
-
-    assert board.code_length == 4
-
-
 class Board:
     """
 
@@ -14,3 +8,9 @@ class Board:
         self.num_colours = num_colours
         self.code_length = len(self.code)
 
+
+if __name__ == '__main__':
+    print("Running")
+    board = Board((1, 2, 1, 3), 5)
+
+    assert board.code_length == 4
