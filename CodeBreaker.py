92a35fae24fab0ae7eb0e8605ce711ac234a587a
BigPoppaG <daniel.gelderd@gmail.com>
2019-06-15 10:12:33 +0100

    Added code length property

--- CodeBreaker.py
+++ CodeBreaker.py
@@ -1,16 +1,7 @@
-import itertools
-import time
-
-
 def test():
     board = Board((1, 2, 1, 3), 5)
 
-    assert powerset([1, 2, 3]) == {(1,), (2,), (3,), (1, 2), (1, 3), (2, 3)}
-    assert board.get_neighbours((1, 1)) == {(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)}
-
-
-
-
+    assert board.code_length == 4
 
 
 class Board:
@@ -21,4 +12,5 @@ class Board:
     def __init__(self, code, num_colours):
         self.code = code
         self.num_colours = num_colours
+        self.code_length = len(self.code)
 
