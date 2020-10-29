18f4075696a91652a3768b81dcf429971b6004b1
BigPoppaG <daniel.gelderd@gmail.com>
2020-10-29 16:01:51 +0000

    Game running and counting blacks

--- CodeBreaker.py
+++ CodeBreaker.py
@@ -1,16 +1,50 @@
+import random
+
+
 class Board:
     """
 
     """
 
-    def __init__(self, code, num_colours):
-        self.code = code
+    def __init__(self, code_length, num_colours, code=None):
         self.num_colours = num_colours
-        self.code_length = len(self.code)
+        self.code_length = code_length
+        if code is None:
+            self.code = self.create_new_code()
+        else:
+            self.code = code
+
+    def play(self):
+        correct = False
+        while not correct:
+            raw_guess = input("Guess the code. Select " + str(self.code_length) + " integers between 1 and "
+                              + str(self.num_colours) + " separated with commas \n")
+            self.check_guess_format(raw_guess)
+            print(self.code)
+            guess = self.guess_to_tuple(raw_guess)
+            self.response(guess)
+            correct = guess == self.code
+            if correct:
+                print('You win, Dood!')
+
+    def response(self, guess):
+        blacks = [i for i in range(self.code_length) if self.code[i] == guess[i]]
+        print('You got ' + str(len(blacks)) + ' blacks')
+        whites = []
+        return blacks
+
+    def create_new_code(self):
+        return tuple(random.randint(1, self.num_colours) for i in range(self.code_length))
+
+    def guess_to_tuple(self, guess):
+        return tuple(map(int, guess.split(',')))
+
+    def check_guess_format(self, raw_guess):
+        return False
 
 
 if __name__ == '__main__':
     print("Running")
-    board = Board((1, 2, 1, 3), 5)
-
+    board = Board(4, 6)
+    board.play()
     assert board.code_length == 4
